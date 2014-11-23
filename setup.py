7380d37a1c9ccc166c2a69e642ee11c34c718c8b
Philip Neustrom <philipn@gmail.com>
2014-11-22 22:43:00 -0800

    custom3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django_xsession',
-      version='0.1-custom2',
+      version='0.1-custom3',
       description='Django middleware that allows cookie sharing across multiple domains.',
       author='Factor AG',
       author_email='webmaster@factor.ch',
